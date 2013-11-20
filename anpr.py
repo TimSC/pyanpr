5f6499709d46de68272e1734cc296b25f996cfe0
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-11-20 21:20:35 +0000

    Start to implement zhai et al

--- anpr.py
+++ anpr.py
@@ -2,16 +2,27 @@
 import scipy.misc as misc
 import scipy.signal as signal
 import numpy as np
+import skimage.morphology as morph
 
 if __name__ == "__main__":
 	im = misc.imread("../pyanpr-data/56897161_d613d63bce_b.jpg")
 	greyim = 0.2126 * im[:,:,0] + 0.7152 * im[:,:,1] + 0.0722 * im[:,:,2]
 
-	vkernel = np.array([[-0.5,-1.,-0.5],[0.,0.,0.],[0.5,1.,0.5]])
-	filterim = signal.convolve2d(greyim, vkernel, mode="same")
-	misc.imsave("vim.png", filterim)
+	#Improved number plate localisation algorithm and its efficient 
+	#field programmable gate arrays implementation
+	#Xiaojun Zhai, Faycal Bensaali, Soodamani Ramalingam
 
-	hkernel = np.array([[-0.5,0.,0.5],[-1,0.,1],[-0.5,0,0.5]])
-	filterim2 = signal.convolve2d(greyim, hkernel, mode="same")
-	misc.imsave("him.png", filterim2)
+	#Highlight number plate
+	imnorm = greyim / 255.
+	se = np.ones((3, 30))
+	opim = morph.opening(imnorm, se)
+
+	diff = greyim - opim + 128.
+
+	#
+
+
+	misc.imsave("opim.png", diff)
+
+	
 
