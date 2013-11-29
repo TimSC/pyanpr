4276fcd79bcbedf98064ab019f6ad96be9ea8906
TimSC <tim2009@sheerman-chase.org.uk>
2013-11-29 19:24:10 +0000

    Reworking deshew

--- anpr2.py
+++ anpr2.py
@@ -1,10 +1,16 @@
 import pickle, sys, math
 import scipy.misc as misc
+import skimage.filter as filt
 from skimage.transform import hough_line, hough_line_peaks
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.ndimage
 
 if __name__=="__main__":
+
+	#Deskew the candidate patch
+	#Based on ALGORITHMIC AND MATHEMATICAL PRINCIPLES OF AUTOMATIC NUMBER PLATE RECOGNITION SYSTEMS 
+	#ONDREJ MARTINSKY
 
 	finaIm = None
 	finaDat = None
@@ -23,16 +29,37 @@ if __name__=="__main__":
 	bbox = roi[2]
 	print bbox
 
+	#Expand bbox
+	mid = [sum(comp)/len(comp) for comp in bbox]
+	print "mid", mid
+	xd = (mid[0] - bbox[0][0]) * 0.2
+	yd = (mid[1] - bbox[1][0]) * 0.2
+	bbox = [[bbox[0][0]-xd, bbox[0][1]+xd], [bbox[1][0]-yd, bbox[1][1]+yd]]
+	print bbox
+
+	#Crop image
 	im = im[bbox[1][0]:bbox[1][1],:]
 	im = im[:,bbox[0][0]:bbox[0][1]]
+	print im.shape
 
+	#Convert to grey
 	misc.imsave("test.png", im)
 	greyim = 0.2126 * im[:,:,0] + 0.7152 * im[:,:,1] + 0.0722 * im[:,:,2]
 
-	h, theta, d = hough_line(greyim)
+	edgeIm = scipy.ndimage.sobel(greyim, axis=0)
+	misc.imsave("test2.png", edgeIm)
+	edgeIm = np.abs(edgeIm.astype(np.float)-128.)
+	misc.imsave("test3.png", edgeIm)
+
+	
+	midThresh = 0.5 * (edgeIm.min() + edgeIm.max())
+	thresholdIm = (edgeIm > midThresh)
+	misc.imsave("test4.png", thresholdIm)
+
+	h, theta, d = hough_line(thresholdIm)
 
 	if 0:
-		plt.imshow(np.log(1 + h),
+		plt.imshow(np.log(h+1),
 		       extent=[np.rad2deg(theta[-1]), np.rad2deg(theta[0]),
 		               d[-1], d[0]],
 		       cmap=plt.cm.gray, aspect=1/1.5)
@@ -40,17 +67,20 @@ if __name__=="__main__":
 		plt.xlabel('Angles (degrees)')
 		plt.ylabel('Distance (pixels)')
 		plt.show()
+	
+	angsum = np.sum(np.power(h, 2.0),axis=0)
+	#print angsum
+	#plt.plot(angsum)
+	#plt.show()
+	bestInd = angsum.argmax()
+	bestAngle = theta[bestInd]
 
-	peaks = hough_line_peaks(h, theta, d)
-	for _, angle, dist in zip(*peaks):
-		print "peak", angle, math.degrees(angle), dist
-
-	bestAngle = peaks[1][0]
 	while bestAngle > math.pi / 4.:
-		bestAngle -= math.pi / 2.
-	while bestAngle < -math.pi / 4.:
-		bestAngle += math.pi / 2.
+		bestAngle -= math.pi /2.
+	while bestAngle < -math.pi /4.:
+		bestAngle += math.pi /2.
 
+	print bestInd, bestAngle, math.degrees(bestAngle)
 	pickle.dump((bbox, bestAngle), open("out.deskew", "wb"), protocol=-1)
 
 
