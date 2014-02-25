407a5a08e1ca2a4d32704cc1fa4f10a1c3017e1c
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2014-02-25 14:14:22 +0000

    Fix single channel of number plate hsv

--- deskewMarkedPlates.py
+++ deskewMarkedPlates.py
@@ -1,6 +1,8 @@
 
 import readannotation, deskew
 import scipy.misc as misc
+import skimage.color as color
+import numpy as np
 
 if __name__=="__main__":
 	plates = readannotation.ReadPlateAnnotation("plates.annotation")
@@ -21,5 +23,30 @@ if __name__=="__main__":
 			bbox, bestInd, bestAngle, rotIm = deskew.Deskew(im, (xran, yran))
 
 			misc.imsave("rotIm{0}.png".format(count), rotIm)			
+
+			hsvImg = color.rgb2hsv(rotIm)
+
+			#Remove hue information
+			fixH = hsvImg.copy()
+			fixH[:,:,0] = np.zeros((fixH.shape[0], fixH.shape[1]))
+			fixHrgb = color.hsv2rgb(fixH)
+
+			misc.imsave("fixH{0}.png".format(count), fixHrgb)
+
+			#Remove saturation information
+			fixS = hsvImg.copy()
+			fixS[:,:,1] = np.ones((fixS.shape[0], fixS.shape[1])) * 128.
+			fixSrgb = color.hsv2rgb(fixS)
+
+			misc.imsave("fixS{0}.png".format(count), fixSrgb)
+
+			#Remove intensity information
+			fixV = hsvImg.copy()
+			fixV[:,:,2] = np.ones((fixV.shape[0], fixV.shape[1])) * 128.
+			fixVrgb = color.hsv2rgb(fixV)
+
+			misc.imsave("fixV{0}.png".format(count), fixVrgb)
+
+
 			count += 1
 
