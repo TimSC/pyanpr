97fba0d611983002bc91484baa6238d8d65dc128
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2014-02-26 16:29:30 +0000

    Analysis of area growth

--- detectblobs.py
+++ detectblobs.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 
 	normIm = exposure.rescale_intensity(scoreIm)
 
-	thresholds = np.linspace(normIm.min(), normIm.max(), 10.)
+	thresholds = np.linspace(normIm.min(), normIm.max(), 100.)
 
 	#Find blobs at various thresolds and calculate area
 	thresholdBlobSizes = []
@@ -57,7 +57,12 @@ if __name__ == "__main__":
 		#numberedRegions, maxRegionNum = morph.label(thresholdIm, 4, 0, return_num = True)
 		#misc.imshow(numberedRegions==seedInRegion)
 	stabilityIm = np.empty(normIm.shape, dtype=np.uint8)
-	
+	import matplotlib.pyplot as plt	
+	testPts = [(50, 50), (75, 22), (222,22), (209,24), (198,55)]
+	testPtsTxt = ['clear', 'clear', 'abig', 'clear', 'background']
+
+	maxLetterArea = 0.05
+
 	for x in range(normIm.shape[1]):
 		for y in range(normIm.shape[0]):
 			areaProfile = []
@@ -67,20 +72,36 @@ if __name__ == "__main__":
 				if ptInRegion != -1:
 					areaProfile.append(blobSizes[ptInRegion])
 				else:
-					areaProfile.append(None)
+					areaProfile.append(0.)
+
+			areaProfile = np.array(areaProfile)
+			areaProfileDiff = areaProfile[1:] - areaProfile[:-1]
+			areaProfileDiff /= numberedRegions.size
+
+			try:
+				testIndex = testPts.index((x,y))
+			except:
+				testIndex = -1
+			if testIndex != -1:
+				
+				#print x, y, areaProfile, numberedRegions.size
+				plt.plot(thresholds, areaProfile / numberedRegions.size, label=testPtsTxt[testIndex])
+
 
 			#print x, y, areaProfile, numberedRegions.size
 
 			scoringThresholds = 0
 			for val in areaProfile:
 				if val is None: continue
-				if val > 0.3 * numberedRegions.size: continue
+				if val > 0.5 * numberedRegions.size: continue
 				scoringThresholds += 1
 
 			#print x, y, scoringThresholds
 
 			stabilityIm[y, x] = scoringThresholds
 
+	plt.legend(loc='upper right')
+	plt.show()
 	stabilityIm = exposure.rescale_intensity(stabilityIm)
 	#misc.imshow(stabilityIm)
 	misc.imsave("test.png", stabilityIm)
