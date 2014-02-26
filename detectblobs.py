75e137e6fb92734c2a7d50a5e2910a5137971bbe
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2014-02-26 17:11:04 +0000

    Modularise blob detection

--- detectblobs.py
+++ detectblobs.py
@@ -5,30 +5,10 @@ import numpy as np
 import skimage.exposure as exposure
 import skimage.morphology as morph
 
-if __name__ == "__main__":
-	finaIm = None
-	finaDeskew = None
-
-	if len(sys.argv) >= 2:
-		finaIm = sys.argv[1]
-	if len(sys.argv) >= 3:
-		finaDeskew = sys.argv[2]
-
-	finaImSplit = os.path.splitext(finaIm)
-	if finaDeskew is None:
-		finaDeskew = finaImSplit[0] +".deskew"
-
-	im = misc.imread(finaIm)
-	bbox, angle = pickle.load(open(finaDeskew))
-	
-
-	rotIm = deskew.RotateAndCrop(im, bbox, angle)
-	
-	scoreIm = deskewMarkedPlates.RgbToPlateBackgroundScore(rotIm)
-
+def FindBlobs(scoreIm, numThresholds = 100, maxLetterArea = 0.05):
 	normIm = exposure.rescale_intensity(scoreIm)
 
-	thresholds = np.linspace(normIm.min(), normIm.max(), 100.)
+	thresholds = np.linspace(normIm.min(), normIm.max(), numThresholds)
 
 	#Find blobs at various thresolds and calculate area
 	thresholdBlobSizes = []
@@ -56,53 +36,66 @@ if __name__ == "__main__":
 		#thresholdIm = normIm < threshold
 		#numberedRegions, maxRegionNum = morph.label(thresholdIm, 4, 0, return_num = True)
 		#misc.imshow(numberedRegions==seedInRegion)
-	stabilityIm = np.empty(normIm.shape, dtype=np.uint8)
-	import matplotlib.pyplot as plt	
-	testPts = [(50, 50), (75, 22), (222,22), (209,24), (198,55)]
-	testPtsTxt = ['clear', 'clear', 'abig', 'clear', 'background']
 
-	maxLetterArea = 0.05
+	bestThreshold = None
+	bestRegionSum = None
+	bestNumberedRegions = None
 
-	for x in range(normIm.shape[1]):
-		for y in range(normIm.shape[0]):
-			areaProfile = []
-			for threshold, blobSizes, numberedRegions in zip(thresholds, thresholdBlobSizes, numberedRegionIms):
-				ptInRegion = numberedRegions[y, x]
-				#print x, y, ptInRegion
-				if ptInRegion != -1:
-					areaProfile.append(blobSizes[ptInRegion])
-				else:
-					areaProfile.append(0.)
+	for threshold, blobSizes, numberedRegions in zip(thresholds, thresholdBlobSizes, numberedRegionIms):
+		count = 0
+		validRegionNums = []
+		validRegionSizes = []
+		for regionNum, blobSize in enumerate(blobSizes):
+			if float(blobSize) / normIm.size < maxLetterArea:
+				count += 1
+				validRegionNums.append(regionNum)
+				validRegionSizes.append(blobSize)
 
-			areaProfile = np.array(areaProfile)
-			areaProfileDiff = areaProfile[1:] - areaProfile[:-1]
-			areaProfileDiff /= numberedRegions.size
+		#print threshold, count, len(blobSizes), validRegionSizes
+		
+		areaSum = sum(validRegionSizes)
+		if bestThreshold is None or areaSum > bestRegionSum:
+			bestRegionSum = areaSum
+			bestThreshold = threshold
+			bestNumberedRegions = numberedRegions
 
-			try:
-				testIndex = testPts.index((x,y))
-			except:
-				testIndex = -1
-			if testIndex != -1:
-				
-				#print x, y, areaProfile, numberedRegions.size
-				plt.plot(thresholds, areaProfile / numberedRegions.size, label=testPtsTxt[testIndex])
+		#if len(validRegionSizes) > 0:
+		#	import matplotlib.pyplot as plt
+		#	plt.hist(validRegionSizes)
+		#	plt.show()
+
+		#if count > 0:
+		#	print threshold, validRegionNums
+		#	for vr in validRegionNums:
+		#		misc.imshow(numberedRegions == vr)
+
+	print "best", bestThreshold, bestRegionSum
+	return bestNumberedRegions
 
 
-			#print x, y, areaProfile, numberedRegions.size
+if __name__ == "__main__":
+	finaIm = None
+	finaDeskew = None
 
-			scoringThresholds = 0
-			for val in areaProfile:
-				if val is None: continue
-				if val > 0.5 * numberedRegions.size: continue
-				scoringThresholds += 1
+	if len(sys.argv) >= 2:
+		finaIm = sys.argv[1]
+	if len(sys.argv) >= 3:
+		finaDeskew = sys.argv[2]
 
-			#print x, y, scoringThresholds
+	finaImSplit = os.path.splitext(finaIm)
+	if finaDeskew is None:
+		finaDeskew = finaImSplit[0] +".deskew"
 
-			stabilityIm[y, x] = scoringThresholds
+	im = misc.imread(finaIm)
+	bbox, angle = pickle.load(open(finaDeskew))
+	
 
-	plt.legend(loc='upper right')
-	plt.show()
-	stabilityIm = exposure.rescale_intensity(stabilityIm)
-	#misc.imshow(stabilityIm)
-	misc.imsave("test.png", stabilityIm)
+	rotIm = deskew.RotateAndCrop(im, bbox, angle)
+	
+	scoreIm = deskewMarkedPlates.RgbToPlateBackgroundScore(rotIm)
+
+	bestNumberedRegions = FindBlobs(scoreIm)
+
+	numberedRegionsIm = exposure.rescale_intensity(bestNumberedRegions != -1)
+	misc.imshow(numberedRegionsIm)
 
