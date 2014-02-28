8aab073c234c4569741293ea3474c85287086d79
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2014-02-28 13:59:27 +0000

    Save resized chars to training file

--- trainchars.py
+++ trainchars.py
@@ -1,7 +1,9 @@
-import readannotation, os, pickle
+import readannotation, os, pickle, random, cStringIO
 import scipy.misc as misc
 import deskew, deskewMarkedPlates, detectblobs
 import numpy as np
+import skimage.exposure as exposure
+from PIL import Image
 
 def ViewPlate(fina, bbox, angle, charBboxes, charCofG):
 
@@ -58,6 +60,30 @@ def StripInternalSpaces(strIn):
 			out.append(ch)
 	return "".join(out)
 
+def ExtractPatch(image, bbox):
+	bbox = map(int,map(round,bbox))
+	#print image.shape, bbox
+	out = np.zeros((bbox[3]-bbox[2], bbox[1]-bbox[0], 3), dtype=image.dtype)
+
+	origin = [0, 0]
+	if bbox[0] < 0:
+		origin[0] = -bbox[0]
+		bbox[0] = 0
+	if bbox[2] < 0:
+		origin[1] = -bbox[2]
+		bbox[2] = 0
+	if bbox[1] >= image.shape[1]:
+		bbox[1] = image.shape[1]-1
+	if bbox[3] >= image.shape[0]:
+		bbox[3] = image.shape[0]-1
+	
+	h = bbox[3]-bbox[2]
+	w = bbox[1]-bbox[0]
+
+	#print bbox
+	out[origin[1]:origin[1]+h, origin[0]:origin[0]+w, :] = image[bbox[2]:bbox[2]+h, bbox[0]:bbox[0]+w, :]
+	return out
+
 if __name__=="__main__":
 	plates = readannotation.ReadPlateAnnotation("plates.annotation")
 	count = 0
@@ -66,6 +92,7 @@ if __name__=="__main__":
 	plateCharCofGs = {}
 	plateCharBboxAndAngle = {}
 	plateString = {}
+	model = {}
 
 	print "Loading"
 	plateCharBboxes = pickle.load(open("charbboxes.dat", "r"))
@@ -76,17 +103,31 @@ if __name__=="__main__":
 
 	objIds = plateString.keys()
 	objIds.sort()
-	for objId in objIds:
+
+	#Use half for training
+	random.shuffle(objIds)
+	splitInd = int(round(len(objIds) / 2.))
+	trainObjIds = objIds[:splitInd]
+	testObjIds = objIds[splitInd:]
+	print "Total plates:", len(objIds)
+	print "Training with",len(trainObjIds),"plates"
+	print "Test with",len(testObjIds),"plates"
+
+	for objId in trainObjIds:
 		for photoNum, photo in enumerate(plates):
 			fina = photo[0]['file']
 			foundObjId = photo[1]['object']
 			if foundObjId == objId:
 				break
 
+		if objId not in plateString: continue #Plate not checked
+
 		bboxes = plateCharBboxes[objId]
 		plateStr = plateString[objId]
 		charCofG = plateCharCofGs[objId]
 		bbox, angle	= plateCharBboxAndAngle[objId]
+
+		if plateStr is None: continue #Bad plate
 
 		plateStrStrip = StripInternalSpaces(plateStr)
 
@@ -94,5 +135,40 @@ if __name__=="__main__":
 		if plateStrStrip is not None and len(plateStrStrip) != len(bboxes):
 			print "Bbox number mismatch"
 
-		ViewPlate(fina, bbox, angle, bboxes, charCofG)
+		#ViewPlate(fina, bbox, angle, bboxes, charCofG)
+		im = misc.imread(fina)
+		rotIm = deskew.RotateAndCrop(im, bbox, angle)
+		
+		for char, bbx, cCofG in zip(plateStrStrip, bboxes, charCofG):
+			print char, cCofG, bbx
+			originalHeight = bbx[3] - bbx[2]
+			scaling = 50. / originalHeight
+			print scaling
+			margin = 40. / scaling
+
+			patch = ExtractPatch(rotIm, (cCofG[1]-margin, cCofG[1]+margin, cCofG[0]-margin, cCofG[0]+margin))
+			
+			#Scale height
+			resizedPatch = misc.imresize(patch, (int(round(patch.shape[0]*scaling)), 
+				int(round(patch.shape[1]*scaling)), patch.shape[2]))
+
+			normIm = exposure.rescale_intensity(resizedPatch)
+
+			print normIm.shape
+			#misc.imshow(normIm)
+
+			if char not in model:
+				model[char] = []
+			pilImg = misc.toimage(resizedPatch)
+			binImage = cStringIO.StringIO()
+			pilImg.save(binImage, "png")
+
+			#Merge I into 1 and O into 0
+			modChar = char
+			if modChar == "I": modChar = "1"
+			if modChar == "O": modChar = "0"
+
+			model[char].append(binImage.getvalue())
+
+	pickle.dump((trainObjIds, testObjIds, model), open("charmodel.dat", "wb"), protocol=-1)
 
